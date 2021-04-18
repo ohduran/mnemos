00c977d4874e38df1497463d33fd90460be09f42
ohduran <alvaro.duranb@hotmail.com>
2021-04-18 17:32:00 +0200

    flipcard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,82 @@
 import React from "react"
 import { BoatAnimated, Helm, Sailor } from "../icons"
+import * as flipcardStyles from "../styles/flipcard.module.css"
 
 export default function Home() {
   return (
     <div
-      className="min-h-screen min-w-screen bg-nord-0 grid gap-2 grid-cols-3 pt-2"
+      className="min-h-screen min-w-screen grid grid-cols-3"
       style={{
         gridTemplateRows: "max-content 1fr 1fr",
       }}
     >
-      <nav className="col-start-1 col-span-3 flex items-baseline px-2">
-        <Helm className="h-10 w-10" />
-        <h1 className="pl-2 text-nord-13 text-4xl font-semibold font-family-secondary">
-          Nemos
-        </h1>
-      </nav>
-      <main className="bg-nord-1 col-start-1 row-start-2 col-span-3 py-5 flex justify-around">
-        <div className="self-center">
-          <h2 className="text-9xl font-family-secondary font-semibold">
-            ¡Al abordaje!
-          </h2>
-          <p className="text-xl pt-16">
-            La manera más rápida y fácil de aprobar el PER.
-          </p>
+      <nav
+        className="col-start-1 col-span-3 grid items-center justify-between pt-2 px-2 pb-2 shadow-lg bg-michelangelo-blue"
+        style={{
+          gridTemplateColumns: "max-content 1fr max-content",
+        }}
+      >
+        <div className="col-start-1 flex items-center">
+          <Helm className="h-10 w-10" />
+          <h1 className="pl-2 text-nord-13 text-4xl font-bold font-family-secondary text-michelangelo-brown">
+            Nemos
+          </h1>
         </div>
-        <Sailor className="pl-5 h-96 w-96" primaryColor="#88c0d0" />
+        <ul className="col-start-3 flex justify-around mr-5 text-michelangelo-white font-semibold">
+          <li>Conócenos</li>
+          <li className="ml-5">Contáctanos</li>
+        </ul>
+      </nav>
+      <main className="col-start-1 row-start-2 col-span-3 pt-32bg-gradient-to-tr from-michelangelo-white via-michelangelo-white to-michelangelo-blue">
+        <section className="grid grid-cols-3 h-screen">
+          <div className="self-center justify-self-start p-16 pt-32 col-start-1 col-span-2 text-michelangelo-orange">
+            <h1 className="text-8xl font-semibold font-family-secondary">
+              ¡Al abordaje!
+            </h1>
+            <p className="mt-5">This text will get everyone to sign up</p>
+            <div>
+              <div
+                className={`${flipcardStyles.cardContainer} mr-20`}
+                style={{
+                  perspective: "9009x",
+                }}
+              >
+                <div
+                  className={`${flipcardStyles.card} relative w-32 h-32 transition-all transform text-center`}
+                >
+                  <div
+                    className={`${flipcardStyles.front} absolute bg-michelangelo-green top-0 left-0 w-32 h-32 rounded-lg text-michelangelo-white shadow-xl flex items-center justify-center text-xl`}
+                    style={{
+                      backfaceVisibility: "hidden",
+                    }}
+                  >
+                    <span className="">¿Cuál es la capital de España?</span>
+                  </div>
+                  <div
+                    className={`${flipcardStyles.back} absolute bg-michelangelo-green top-0 left-0 w-32 h-32 rounded-lg text-michelangelo-orange shadow-xl flex items-center justify-center text-lg`}
+                    style={{
+                      backfaceVisibility: "hidden",
+                    }}
+                  >
+                    Madrid
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Sailor
+            className="py-5 h-full w-96 row-start-1 col-start-3 col-span-1 self-center justify-self-center z-10"
+            primaryColor="#42819F"
+          />
+        </section>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#42819F"
+            fill-opacity="1"
+            d="M0,128L120,149.3C240,171,480,213,720,192C960,171,1200,85,1320,42.7L1440,0L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+          ></path>
+        </svg>
+        <section className=""></section>
       </main>
     </div>
   )
